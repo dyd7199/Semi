0704ed9ecb2e1279cf85957752452d631cea3cba
HYELIM <hyerimm1127@gmail.com>
2021-04-23 09:54:45 +0900

    수정

--- SemiProject/src/service/face/MemberService.java
+++ SemiProject/src/service/face/MemberService.java
@@ -60,12 +60,7 @@ public interface MemberService {
 	 * @return
 	 */
 	public Member getuserInfo(Member member);
-	
-	/**
-	 * 레시피 게시글에 보여질 모든 회원정보 데이터
-	 * 
-	 * @return 모든 회원정보 데이터
-	 */
+
 	public List<Member> getUserdata();
 
 	/**
@@ -114,28 +109,6 @@ public interface MemberService {
 	 * @param member
 	 */
 	public void chagePW(Member member);
-	
-	/**
-	 * session으로부터 받은 회원번호와 레시피 작성자의 회원번호를 비교
-	 * 
-	 * @param req
-	 * @return 일치할 경우 true, 불일치할 경우 false 반환
-	 */
-	public boolean UsernoChk(HttpServletRequest req);
-
-	/**
-	 * 아이디 찾기
-	 * @param req
-	 * @return
-	 */
-	public Member findId(HttpServletRequest req);
-
-	/**
-	 * 비밀번호 찾기
-	 * @param req
-	 * @return
-	 */
-	public Member findPw(HttpServletRequest req);
 
 
 	
